01cf10e0a87582ea11c1a0260baefa29d1137ffd
timebus-org <timebus.in@gmail.com>
2026-01-21 21:48:00 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  timeout: 10000
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  timeout: 10000,
 });
